cfef90b36db90e9d8b0bbe948de3db8d8e2fb06c
Andrew Rowe <rowead@gmail.com>
2017-03-05 11:44:51 +0800

    improving shell script, now you just need to provide xlsx file as parameter.

--- xlsx2csv.sh
+++ xlsx2csv.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-/opt/data-integration/pan.sh -file=xlsx2csv.ktr -param:spreadsheet='${Internal.Transformation.Filename.Directory}${file.separator}'${1}
+extension="${1##*.}"
+file=$(realpath "${1}")
+filename="${file%.*}"
+
+echo ${filename}
+if [ ! -f "${file}" ]; then
+  echo "File does not exist"
+  exit 1;
+fi
+
+if [ "${extension}" != "xlsx" ]; then
+  echo "Not an xlsx file"
+  exit 1
+fi
+/opt/data-integration/pan.sh -file=xlsx2csv.ktr -param:spreadsheet="${filename}"
